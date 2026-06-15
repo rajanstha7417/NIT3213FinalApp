78f946e9c69317e83b8a7cdfbaa73b160b8f4576
Rajan Shrestha <datawithrajan7417@gmail.com>
2026-06-15 13:27:22 +1000

    Set up Gradle project with wrapper and build configuration

---
+++ build.gradle.kts
@@ -0,0 +1,6 @@
+// Top-level build file. Plugins are declared here and applied in the app module.
+plugins {
+    id("com.android.application") version "8.5.2" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.24" apply false
+    id("com.google.dagger.hilt.android") version "2.51.1" apply false
+}
